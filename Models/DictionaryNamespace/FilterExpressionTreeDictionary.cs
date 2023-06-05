ad4cc8620b75e4f44ce2a220b713ac66677f18de
Skylar Kenneth <azva_s_kenneth@yahoo.com>
2023-06-04 20:20:22 -0700

    Add basic migration and tracking functionality for Tree Dictionary

--- Models/DictionaryNamespace/FilterExpressionTreeDictionary.cs
+++ Models/DictionaryNamespace/FilterExpressionTreeDictionary.cs
@@ -7,24 +7,67 @@ using System.Threading.Tasks;
 
 namespace Models.DictionaryNamespace
 {
-    public class FilterExpressionTreeDictionary : Dictionary<string, FilterExpressionTree>, IDictionary<string, FilterExpressionTree>
+    public class FilterExpressionTreeDictionary
     {
         private readonly IDictionary<string, FilterExpressionTree> _treeDictionary = new Dictionary<string, FilterExpressionTree>();
+        private List<string> _rootKeys = new List<string>();
 
-        public FilterExpressionTree? this[string root, string key]
+        public FilterExpressionTree? this[string key]
         {
-            get { return _treeDictionary[root]; }
-            set 
-            { 
-                if(string.IsNullOrWhiteSpace(root))
-                    throw new ArgumentNullException(nameof(root));
-                if(string.IsNullOrWhiteSpace(key))
+            get => _treeDictionary.ContainsKey(key) ? _treeDictionary[key] : null;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(key))
                     throw new ArgumentNullException(nameof(key));
-                _treeDictionary[$"{root.Trim()}:{key.Trim()}"] = value; 
+                var rootKey = ExtractRootKey(key);
+                if (!_rootKeys.Contains(rootKey))
+                    _rootKeys.Add(rootKey);
+                _treeDictionary[key] = value;
             }
         }
-        
-        public new void Add(string rootName, string key, FilterExpressionTree treeNode)
-            => this[rootName, key] = treeNode;
+
+        public FilterExpressionTree? FindNode(FilterExpressionTree root, string childNode)
+            => (string.IsNullOrWhiteSpace(root?.Expression?.Name) || string.IsNullOrWhiteSpace(childNode)
+            ? null
+            : this[$"{ExtractRootKey(root.Expression.Name)}:{childNode}"]);
+
+        private string? ExtractRootKey(string key) => key.Split(":").FirstOrDefault();
+        private string? ExtractNonRootKey(string key) => string.Join(":", key.Split(':').Skip(1));
+        private string? CreateRootKey(string? rootKey, string? nodeKey) => (string.IsNullOrWhiteSpace(rootKey + nodeKey)) ? null : $"{rootKey}:{nodeKey}";
+        private string? CreateMigrateKey(string? rootKey, string? existingKey) => (string.IsNullOrWhiteSpace(rootKey + existingKey)) ? null : $"{rootKey}:{ExtractNonRootKey(existingKey)}";
+
+        //SourceRootLabel:ChildLabel => TargetRootLabel:ChidLabel
+        public void UpdateRoot(string rootSource, string rootTarget)
+        {
+            //Source is the old tree whose children nodes need to be updated,
+            //While Target node is a leaf node of a parent tree node which is connecting to Source tree
+
+            //Get root keys
+            var extractedSourceKey = ExtractRootKey(rootSource);
+            var extractedTargetKey = ExtractRootKey(rootTarget);
+            //Get source nodes
+            var rootSourceNodes = _treeDictionary.Where(node => ExtractRootKey(node.Key) == extractedSourceKey);
+            //Get target root to connect to source nodes
+            var rootTargetNode = _treeDictionary.FirstOrDefault(node => ExtractRootKey(node.Key) == extractedTargetKey && node.Value.Parent == null);
+
+            rootSourceNodes.ToList().ForEach(node =>
+            {
+                //Update Keys from source tree to target tree
+                _treeDictionary.Remove(node.Key);
+                var targetKey = CreateMigrateKey(extractedTargetKey, node.Key);
+                _treeDictionary.TryAdd(targetKey, node.Value);
+            });
+            _rootKeys.Remove(extractedSourceKey);
+        }
+        public void Add(string? rootKey, string? key, FilterExpressionTree treeNode)
+            => this[CreateRootKey(rootKey, key)] = treeNode;
+        private void MigrateTreeNode(string? rootKey, FilterExpressionTree node)
+        {
+            if(string.IsNullOrWhiteSpace(node?.Expression?.Name))
+                throw new ArgumentNullException(nameof(node), $"Migrating node may be null. Unable to construct key to migrate to '{ExtractRootKey(rootKey)}' tree");
+            if(string.IsNullOrWhiteSpace(rootKey))
+                throw new ArgumentNullException(nameof(rootKey), $"Root key is null. Cannot migrate node {node.Expression.Name}");
+            var existingRoot = _rootKeys.FirstOrDefault(x => x == ExtractRootKey(rootKey));
+        }
     }
 }
