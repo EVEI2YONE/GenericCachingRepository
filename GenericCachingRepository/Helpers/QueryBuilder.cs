b92ae207511700fc6fc57b278836ecb07960d374
Skylar Kenneth <azva_s_kenneth@yahoo.com>
2024-02-24 21:11:36 -0700

    comment out query builder extension

---
+++ GenericCachingRepository/Helpers/QueryBuilder.cs
@@ -0,0 +1,138 @@
+﻿using GenericCachingRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCachingRepository.Helpers
+{
+    //public static class QueryBuilder
+    //{
+    //    private static string GetName<T>(Expression<Func<T, object>> exp) where T : class
+    //    {
+    //        MemberExpression body = exp.Body as MemberExpression;
+
+    //        if (body == null)
+    //        {
+    //            UnaryExpression ubody = (UnaryExpression)exp.Body;
+    //            body = ubody.Operand as MemberExpression;
+    //        }
+
+    //        return body.Member.Name;
+    //    }
+
+    //    public static Tuple<List<Where>, string> WhereBy<T>(Expression<Func<T, object>> expressionName) where T : class
+    //    {
+    //        return WhereBy_Common(expressionName);
+    //    }
+
+    //    public static Tuple<List<Where>, string> WhereBy_Common<T>(this WhereGroup whereBy, Expression<Func<T, object>> expressionName) where T : class
+    //    {
+    //        return WhereBy_Common(expressionName);
+    //    }
+
+    //    private static Tuple<List<Where>, string> WhereBy_Common<T>(this WhereGroup whereBy, Expression<Func<T, object>> expressionName) where T : class
+    //    {
+    //        var specificClause = new List<Where>();
+    //        var name = GetName<T>(expressionName);
+    //        return new Tuple<List<Where>, string>(specificClause, name);
+    //    }
+
+    //    public static Tuple<List<Where>, string> OrWhereBy<T>(this WhereGroup whereBy, Expression<Func<T, object>> expressionName) where T : class
+    //    {
+    //        var 
+    //    }
+
+    //    public static Tuple<List<Where>, string> And<T>(this Tuple<List<Where>, string> whereBy, Expression<Func<T, object>> expressionName) where T : class
+    //    {
+    //        var name = GetName<T>(expressionName);
+    //        return new Tuple<List<Where>, string>(whereBy.Item1, name);
+    //    }
+
+    //    public static WhereGroup Build(this Tuple<List<Where>, string> whereBy)
+    //    {
+    //        WhereGroup group = new WhereGroup();
+    //        group.SpecificClauses = whereBy.Item1;
+    //        group.OtherClauses = new List<WhereGroup>();
+    //        return group;
+    //    }
+
+    //    public static Query OrderBy(this Query query, string column, bool ascending = true)
+    //    {
+    //        if(query != null)
+    //        {
+    //            query.Order = query.Order ?? new List<Order>();
+    //            var list = query.Order.ToList();
+    //            list.Add(new Order()
+    //            {
+    //                Column = column,
+    //                SortOrder = ascending ? SortOrder.Asc : SortOrder.Desc
+    //            });
+    //            query.Order = list;
+    //        }
+    //        return query;
+    //    }
+    //}
+
+
+
+    //public static class QueryExpressionBuilder
+    //{
+    //    public static Tuple<List<Where>, string> IsNull(this Tuple<List<Where>, string> whereBy)
+    //    {
+    //        whereBy.Item1.Add(new WhereValue()
+    //        {
+    //            Column = whereBy.Item2,
+    //            Operation = ComparativeOperation.Equal,
+    //            Value = null
+    //        });
+    //        return whereBy;
+    //    }
+
+    //    public static Tuple<List<Where>, string> IsLessThan(this Tuple<List<Where>, string> whereBy, object? value)
+    //    {
+    //        whereBy.Item1.Add(new WhereValue()
+    //        {
+    //            Column = whereBy.Item2,
+    //            Operation = ComparativeOperation.LessThan,
+    //            Value = value?.ToString()
+    //        });
+    //        return whereBy;
+    //    }
+
+    //    public static Tuple<List<Where>, string> IsLessThanOrEqual(this Tuple<List<Where>, string> whereBy, object? value)
+    //    {
+    //        whereBy.Item1.Add(new WhereValue()
+    //        {
+    //            Column = whereBy.Item2,
+    //            Operation = ComparativeOperation.LessThanEqual,
+    //            Value = value?.ToString()
+    //        });
+    //        return whereBy;
+    //    }
+
+    //    public static Tuple<List<Where>, string> IsGreaterThan(this Tuple<List<Where>, string> whereBy, object? value)
+    //    {
+    //        whereBy.Item1.Add(new WhereValue()
+    //        {
+    //            Column = whereBy.Item2,
+    //            Operation = ComparativeOperation.GreaterThan,
+    //            Value = value?.ToString()
+    //        });
+    //        return whereBy;
+    //    }
+
+    //    public static Tuple<List<Where>, string> IsGreaterThanOrEqual(this Tuple<List<Where>, string> whereBy, object? value)
+    //    {
+    //        whereBy.Item1.Add(new WhereValue()
+    //        {
+    //            Column = whereBy.Item2,
+    //            Operation = ComparativeOperation.GreaterThanEqual,
+    //            Value = value?.ToString()
+    //        });
+    //        return whereBy;
+    //    }
+    //}
+}
---
+++ Tests/Model Tests/ExtensionQueryBuilderTests.cs
@@ -0,0 +1,176 @@
+﻿//using DbFirstTestProject.DataLayer.Context;
+//using DbFirstTestProject.DataLayer.Entities;
+//using GenericCachingRepository.Helpers;
+//using GenericCachingRepository.Models;
+
+//namespace Tests.Model_Tests
+//{
+//    internal class ExtensionQueryBuilderTests
+//    {
+//        private EntityProjectContext _context;
+//        private Query _query;
+//        private string col1 = nameof(Table1.Col1_PK);
+//        private string col2 = nameof(Table1.Col2);
+//        private string col3 = nameof(Table1.Col3);
+//        private string col4 = nameof(Table1.Col4);
+
+//        [OneTimeSetUp]
+//        public void OneTimeSetUp()
+//        {
+//            _context = new EntityProjectContext();
+
+//            _query = new Query()
+//            {
+//                Page = 1,
+//                PageSize = 10,
+//                RowCount = 5,
+//                Order = new List<Order>()
+//                {
+//                    new Order()
+//                    {
+//                        Column = col2,
+//                        SortOrder = SortOrder.Asc
+//                    },
+//                    new Order()
+//                    {
+//                        Column = col3,
+//                        SortOrder = SortOrder.Asc
+//                    }
+//                },
+//                Where = new WhereGroup()
+//                {
+//                    SpecificClauses = new List<Where>()
+//                    {
+//                        new WhereValue()
+//                        {
+//                            Column = col1,
+//                            Value = "4144917",
+//                            Operation = ComparativeOperation.LessThan
+//                        },
+//                        new WhereValue()
+//                        {
+//                            Column = col4,
+//                            Value = null,
+//                            Operation = ComparativeOperation.Equal
+//                        }
+//                    },
+//                    OtherClauses = new List<WhereGroup>()
+//                    {
+//                        new WhereGroup()
+//                        {
+//                            SpecificClauses = new List<Where>()
+//                            {
+//                                new WhereValue()
+//                                {
+//                                    Column = col2,
+//                                    Value = "Insert",
+//                                    Operation = ComparativeOperation.Equal
+//                                }
+//                            }
+//                        },
+//                        new WhereGroup()
+//                        {
+//                            SpecificClauses = new List<Where>()
+//                            {
+//                                new WhereRange()
+//                                {
+//                                    Column = col3,
+//                                    Start = "2",
+//                                    End  = "4",
+//                                    Operation = RangeOperation.Between
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            };
+
+//            var (where, order) = _query.Evaluate<Table1>();
+//            var expectedWhere = @$"(Col1_PK < 4144917 and Col4 == null) or (Col2 == ""Insert"") or (Col3 >= 2 and Col3 < 4)";
+//            var expectedOrder = $@"Col2 Asc, Col3 Asc";
+
+//            AssertionHelper.AssertSame(where, expectedWhere);
+//            AssertionHelper.AssertSame(order, expectedOrder);
+//        }
+
+//        [SetUp]
+//        public void SetUp()
+//        {
+
+//        }
+
+//        [Test]
+//        public void Extension_Test()
+//        {
+//            var query = new Query()
+//            {
+//                Page = 1,
+//                PageSize = 10,
+//                RowCount = 5,
+//            };
+
+//            //var where = new WhereGroup()
+//            //{
+//            //    SpecificClauses = new List<Where>()
+//            //    {
+//            //        new WhereValue()
+//            //        {
+//            //            Column = col1,
+//            //            Value = "4144917",
+//            //            Operation = ComparativeOperation.LessThan
+//            //        },
+//            //        new WhereValue()
+//            //        {
+//            //            Column = col4,
+//            //            Value = null,
+//            //            Operation = ComparativeOperation.Equal
+//            //        }
+//            //    },
+//            //    OtherClauses = new List<WhereGroup>()
+//            //    {
+//            //        new WhereGroup()
+//            //        {
+//            //            SpecificClauses = new List<Where>()
+//            //            {
+//            //                new WhereValue()
+//            //                {
+//            //                    Column = col2,
+//            //                    Value = "Insert",
+//            //                    Operation = ComparativeOperation.Equal
+//            //                }
+//            //            }
+//            //        },
+//            //        new WhereGroup()
+//            //        {
+//            //            SpecificClauses = new List<Where>()
+//            //            {
+//            //                new WhereRange()
+//            //                {
+//            //                    Column = col3,
+//            //                    Start = "2",
+//            //                    End  = "4",
+//            //                    Operation = RangeOperation.Between
+//            //                }
+//            //            }
+//            //        }
+//            //    }
+//            //};
+
+//            //(Col1_PK < 4144917 and Col4 is null) or(Col2 = 'Insert') or(Col3 >= 2 and Col3 < 4)
+//            var where = QueryBuilder.WhereBy<Table1>(o => o.Col1_PK)
+//                .IsLessThanOrEqual(4144916)
+//                .And<Table1>(o => o.Col4).IsNull()
+//                .Build();
+
+//            //where.OrWhereBy<Table1>(o => o.Col2)
+//            //    .EqualsTo("Insert")
+//            //    .OrWhereBy<Table1>(o => o.Col3)
+//            //    .IsLessThan(4);
+
+//            query.OrderBy(col1)
+//                 .OrderBy(col2);
+
+//            Assert.True(query.Order?.Any() ?? false);
+//        }
+//    }
+//}
